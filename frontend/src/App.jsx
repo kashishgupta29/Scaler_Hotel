76489e4456e50943926d1fd80a338f725ada2937
kashishgupta29 <kashish.kg11@gmail.com>
2025-09-03 19:36:25 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <BrowserRouter>
-      <Toaster position="top-right" />
+      <Toaster position="top-center" />
       <TopBar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
